e6d158fed77d6836d8e624ab25e307a1abacea08
DinhHongKong <83532836+dinhhongkong@users.noreply.github.com>
2024-08-21 22:21:19 +0700

    update report service

---
+++ ReportingService/src/main/java/org/kong/reportingservice/repository/InvoiceRepository.java
@@ -0,0 +1,20 @@
+package org.kong.reportingservice.repository;
+
+import org.kong.reportingservice.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+    @Query("""
+            select i from Invoice i inner join i.tickets tickets
+            where i.creationDate >= ?1 and i.creationDate <= ?2 and i.status = ?3 and tickets.trip.bus.type.id = ?4""")
+    List<Invoice> findInvoiceByDayAndType(LocalDateTime startOfYear, LocalDateTime endOfYear, Byte status, int typeId);
+
+    @Query("select i from Invoice i where i.status = ?1")
+    List<Invoice> findByStatus(Byte status);
+
+
+}
